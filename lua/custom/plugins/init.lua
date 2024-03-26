930bf8c45ee5466edd00fee9c5f62179f0cda16a
Tc4s3 <136351205+Tc4s3@users.noreply.github.com>
2024-03-26 19:48:02 +0000

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'Mofiqul/dracula.nvim',
+  'nvim-telescope/telescope-file-browser.nvim',
+  'anuvyklack/help-vsplit.nvim',
+  'mbbill/undotree',
+}
